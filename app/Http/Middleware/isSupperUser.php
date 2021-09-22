5d1fe4bd58731829409db38e6b1c49cc8ebd4aec
ILY-1606 <no1.ily1606@gmail.com>
2021-09-22 20:25:37 +0700

    add userUser MiddleWare

--- app/Http/Middleware/isSupperUser.php
+++ app/Http/Middleware/isSupperUser.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class IsAdmin
+class isSupperUser
 {
     /**
      * Handle an incoming request.
@@ -17,9 +17,10 @@ class IsAdmin
      */
     public function handle(Request $request, Closure $next)
     {
-        if (Auth::user() &&  (Auth::user()->user_type == "admin" || Auth::user()->user_type == "seller")) {
+        if (Auth::user() &&  Auth::user()->user_type == "admin"){
             return $next($request);
-        } else {
+        }
+        else{
             return redirect('/');
         }
     }
